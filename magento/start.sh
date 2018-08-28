bcf1c85d6aa9ddca86f3240681d9409252ab1415
Denys Gergun <dgergun@magento.com>
2018-08-28 13:05:04 +0300

    Added start script for non-GUI mode

--- magento/start.sh
+++ magento/start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-jmeter -t Checkout.jmx
+#GUI mode
+#jmeter -t Checkout.jmx
+#non-GUI mode
+jmeter -n -t Checkout.jmx -l ../results/magento-150/results.csv -e -o ../results/magento-150/results
